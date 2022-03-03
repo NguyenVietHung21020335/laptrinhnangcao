dc33a53d67cdee5f1899bf857e39481bb06e6699
NguyenVietHung21020335 <100114976+NguyenVietHung21020335@users.noreply.github.com>
2022-03-03 20:44:21 +0700

    Create Bai4.cpp

---
+++ Week2/Bai4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int soCon,soChan;
+    cin>>soCon>>soChan;
+   
+    bool flag = false; 
+    for (int i=1;i<=soCon;i++){
+        if (i*2+(soCon-i)*4==soChan){
+            cout<< "chicken"<<" = "<< i <<", dog ="<<" "<<soCon-i;
+            flag = true;
+            return 0;
+        }
+    }
+    if (flag==false)
+       cout<< "invalid";
+}
+//cách 2
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int soCon,soChan;
+//     cin>>soCon>>soChan;
+   
+//     for (int i=1;i<=soCon;i++){
+//         if (i*2+(soCon-i)*4==soChan){
+//             cout<< "chicken"<<" = "<< i <<", dog ="<<" "<<soCon-i;
+//             return 0;
+//         }
+//     }
+//     cout<< "invalid";
+// }
